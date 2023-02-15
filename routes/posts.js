2485f55eb71d27167944a11d4efb19a479736ad7
hyowony <jas01136@naver.com>
2023-02-15 17:15:02 +0900

    게시글 삭제까지 완성

--- routes/posts.js
+++ routes/posts.js
@@ -43,6 +43,75 @@ router.get("/:id", async (req,res)=> {
   }
   
 })
+router.patch("/:id", async (req,res)=> {
+  const {id} = req.params;
+
+  if (!req.body || !id) {
+    return res.json({messsage: "데이터 형식이 올바르지 않습니다."})
+  }
+  const {user,title, content,password} = req.body;
+  const post = await Post.findById(id);
+
+  if(!post) {
+    return res.json({message:"게시글 조회에 실패했습니다."})
+  }
+  const ispasswordCorrect = post.password === password
+
+  if (ispasswordCorrect) {
+    if(user) {
+      post.user = user;
+    }
+  
+  if (user) {
+    post.user = user;
+  }
+  if (title) {
+    post.title = title
+  }
+  if(content) {
+    post.content = content;
+  }
+  try {
+    const updatePost = await post.save();
+    res.json({date:updatePost});
+  } catch(err) {
+    res.status(500).json({message:err.message});
+  }
+  }else {
+    res.status(401).json({message : "비밀번호가 틀렸습니다"})
+  }
+
+});
+
+//게시글 삭제
+router.delete("/:id",async (req,res)=> {
+  const {id} = req.params;
+  const {password} = req.body;
+
+  const post = await Post.findById(id);
+
+  if(!post) {
+    return res.json({message: "게시글 조회에 실패했습니다"})
+  
+  } const ispasswordCorrect = post.password === password
+
+  if(ispasswordCorrect) {
+    try {
+      await post.remove();
+      res.json({message: "게시글을 삭제했습니다."})
+  
+    }catch(err) {
+      res.status(500).json({message:err.message})
+  
+  
+    }
+  }else {
+    res.status(401).json({message : "비밀번호가 틀렸습니다"})
+
+
+  }  
+ 
+})
 
 module.exports  = router;
 
